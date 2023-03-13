b9e2035da6377e4f237dc829617074c4d533b9c4
Wouter Deconinck <wdconinc@gmail.com>
2023-03-13 12:41:43 -0500

    SPDX header, usage

--- phonebook.sh
+++ phonebook.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2023, Dmitry Arkhipkin, Wouter Deconinck
+
+if [[ $# -lt 1 ]] ; then
+    echo "$(basename $0) - Simple command line interface to the EICUG phonebook"
+    echo ""
+    echo "Usage:  $(basename $0) <name>"
+    exit 0
+fi
+
 IDS=$( curl -s "https://phonebook.sdcc.bnl.gov/eic/service/index.php?q=/service/search/object:members/type:combined/keyword:{$1}/autocomplete:no" | jq -r -c '.[] | .members_id ' )
 
 for i in $IDS
